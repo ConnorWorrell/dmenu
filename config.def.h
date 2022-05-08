cdeb74fa808f17f7d70e472b0773d0684e180d05
Connor Worrell <chemicalcoal1@gmail.com>
2022-05-07 19:08:25 -0600

    updated colors

--- config.def.h
+++ config.def.h
@@ -12,8 +12,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#004d4d" },
+	[SchemeSelHighlight] = { "#ffc978", "#004d4d" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
